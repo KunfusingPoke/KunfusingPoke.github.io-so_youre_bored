aba75c2f2caddd44500dbaceb4fa4be5465109f6
KunfusingPoke <105336828+KunfusingPoke@users.noreply.github.com>
2022-07-17 13:57:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var randomlinks = [];
+randomlinks[0]="http://ducksarethebest.com/";
+randomlinks[1]="http://cant-not-tweet-this.com/";
+randomlinks[2]="https://yyyyyyy.info";
+randomlinks[3]="https://mondrianandme.com";
+randomlinks[4]="http://www.partridgegetslucky.com/";
+randomlinks[5]="http://lastmealsproject.com";
+randomlinks[6]="https://floatingqrcode.com";
+randomlinks[7]="http://just-shower-thoughts.tumblr.com/";
+randomlinks[8]="http://www.fallingfalling.com/";
+randomlinks[9]="http://www.staggeringbeauty.com/";
+randomlinks[10]="https://www.mnpuzzleleague.com";
+
+function randomlink(){
+  window.open(randomlinks[Math.floor(Math.random()*randomlinks.length)]);
+}
+
+var randomlinks1 = [];
+randomlinks1[0]="https://www.reddit.com/r/photoshop/";
+randomlinks1[1]="https://www.reddit.com/r/dontyouknowwhoiam/";
+randomlinks1[2]="https://www.reddit.com/r/CanadaPolitics/";
+randomlinks1[3]="https://www.reddit.com/r/hxcjosh23mealprep/";
+randomlinks1[4]="https://www.reddit.com/r/IllegallySmolCats/";
+randomlinks1[5]="https://www.reddit.com/r/ViralSnaps/";
+randomlinks[6]="https://www.reddit.com/r/dankmemes/";
+randomlinks1[7]="https://www.reddit.com/r/PornhubComments/";
+randomlinks1[8]="https://www.reddit.com/r/Thisismylifemeow/";
+randomlinks1[9]="https://www.reddit.com/r/NotTimAndEric/";
+randomlinks1[10]="https://www.reddit.com/r/Coffee/";
+
+function randomlink1(){
+  window.open(randomlinks1[Math.floor(Math.random()*randomlinks.length)]);
+}
+
+var randomlinks2 = [];
+randomlinks2[0]="https://gypopothomas.itch.io/bioevil4";
+randomlinks2[1]="https://maxhgrobins.itch.io/mondays";
+randomlinks2[2]="https://wenderlygames.itch.io/assessment-examination";
+randomlinks2[3]="https://jstris.jezevec10.com";
+randomlinks2[4]="https://krisjet.itch.io/snek";
+randomlinks2[5]="https://johnsterspacegames.itch.io/baldis-fun-new-school-remastered";
+randomlinks2[6]="https://chloethemonke.itch.io/chance-encounter";
+randomlinks2[7]="https://feraswheel.itch.io/pipsup";
+randomlinks2[8]="https://skribbl.io";
+randomlinks2[9]="https://agar.io";
+randomlinks2[10]="https://emupedia.net/beta/emuos/";
+
+function randomlink2(){
+  window.open(randomlinks2[Math.floor(Math.random()*randomlinks.length)]);
+}
